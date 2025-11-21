1446be2cab89410b032cc96f4baf42e5faf543f8
MiningCableHQ <roserofrank5@gmail.com>
2025-11-21 10:04:02 +0800

    Added Gold Yield after battle Adjusted Gold Yield both loot chests and battle aftermath Adjusted the values for all Weapons

--- src/Game/Chest.java
+++ src/Game/Chest.java
@@ -24,6 +24,7 @@ public abstract class Chest{
 
     public void obtain(Character player){
         player.gainExperience(expYield);
+        System.out.println("\uD83E\uDE99 Gained "+gold+" extra gold coins!");
         player.setCurrency(gold);
     }
 
