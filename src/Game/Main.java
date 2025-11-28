d3cbc57aa576bc67949208f815c3a4c5faef81c1
Kaylooww <kaylocastro1@gmail.com>
2025-11-29 03:29:40 +0800

    Just added ASCII_ART more to come

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -1,5 +1,7 @@
 package Game;
 
+import AsciiArts.ASCII_ART;
+
 public class Main {
     public static void main(String[] args) {
         //Starting the game
@@ -7,8 +9,23 @@ public class Main {
         //TODO add display during handleDefeat() method
         //TODO add display during handleVictory() method
         //Mb gaw ari lang nako i butang kay gipa dali ko I search lang na nga method mao mana ila mga pangan
-        Game game = new Game();
-        game.initializeGame();
-        game.startGame();
+//        Game game = new Game();
+//        game.initializeGame();
+//        game.startGame();
+
+        ASCII_ART asciiArt = new ASCII_ART();
+
+        //Start menu design
+        asciiArt.startMenu();
+
+        //Class arts
+        //TODO implement these when choosing what class you want to play with (in the class file)
+        asciiArt.hawkseyeArt();
+        asciiArt.runeCasterArt();
+        asciiArt.shinobiArt();
+
+        //Character in the original world
+        asciiArt.sageKhai();
+        asciiArt.finalBoss();
     }
 }
